837c0bc5b1f64e05a04ee809bd9770e3c47a03d1
Gerold31 <kramer.leroy@gmail.com>
2013-12-22 13:51:32 +0100

    add ComponentHardware

---
+++ SpaceCraft/include/ComponentHardware.hpp
@@ -0,0 +1,39 @@
+#ifndef _COMPONENTHARDWARE_HPP_
+#define _COMPONENTHARDWARE_HPP_
+
+#include "Component.hpp"
+#include "SpaceCraftDefines.hpp"
+
+namespace SpaceCraft
+{
+
+class ComponentCPU;
+
+class ComponentHardware : public Component
+{
+public:
+    ComponentHardware(Object *object, ParamMap &params, unsigned int id, unsigned int mf, WORD ver);
+    ~ComponentHardware();
+
+    void init();
+    void update(float elapsedTime);
+    void receiveMessage(Message *message);
+
+    virtual void interrupt() = 0;
+
+    WORD getIDL() {return  mID        & 0xFFFF;}
+    WORD getIDH() {return (mID >> 16) & 0xFFFF;}
+    WORD getVER() {return  mVER;}
+    WORD getMFL() {return  mMF        & 0xFFFF;}
+    WORD getMFH() {return (mMF >> 16) & 0xFFFF;}
+
+protected:
+    ComponentCPU *mCPU;
+    const unsigned int mID, mMF;
+    const WORD mVER;
+
+};
+
+};
+
+#endif
---
+++ SpaceCraft/src/ComponentHardware.cpp
@@ -0,0 +1,38 @@
+#include "ComponentHardware.hpp"
+
+#include "TypeInfo.hpp"
+#include "SystemLog.hpp"
+
+using namespace SpaceCraft;
+
+ComponentHardware::ComponentHardware(Object *object, ParamMap &params, unsigned int id, unsigned int mf, WORD ver) :
+    Component(object, params, mType),
+    mID(id), mVER(ver), mMF(mf)
+{
+    LOG_IN("component");
+    LOG_OUT("component");
+}
+
+ComponentHardware::~ComponentHardware()
+{
+    LOG_IN("component");
+    LOG_OUT("component");
+}
+
+void ComponentHardware::init()
+{
+    LOG_IN("component");
+    LOG_OUT("component");
+}
+
+void ComponentHardware::update(float elapsedTime)
+{
+    LOG_IN_FRAME;
+    LOG_OUT_FRAME;
+}
+
+void ComponentHardware::receiveMessage(Message *message)
+{
+    LOG_IN_FRAME;
+    LOG_OUT_FRAME;
+}
