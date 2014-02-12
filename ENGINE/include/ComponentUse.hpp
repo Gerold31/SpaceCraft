0d23594db7ccaad2bc73611251adf092654633d5
Gerold31 <kramer.leroy@gmail.com>
2014-02-12 18:48:15 +0100

    add ComponentUse

---
+++ ENGINE/include/ComponentUse.hpp
@@ -0,0 +1,34 @@
+#ifndef _COMPONENTUSE_HPP_
+#define _COMPONENTUSE_HPP_
+
+#include "Component.hpp"
+
+namespace ENGINE
+{
+
+class ComponentCamera;
+
+class ComponentUse : public Component
+{
+public:
+    ComponentUse(Object *object, ParamMap &params);
+    ~ComponentUse();
+
+    static void *createInstance(Object *object, ParamMap &params);
+    
+    void init();
+    void update(float elapsedTime);
+    void receiveMessage(Message *message);
+
+    static TypeInfo *getType() {return mType;}
+
+private:
+    static TypeInfo *mType;
+
+    ComponentCamera *mParent;
+
+};
+
+};
+
+#endif
---
+++ ENGINE/src/ComponentUse.cpp
@@ -0,0 +1,94 @@
+#include "ComponentUse.hpp"
+
+#include "TypeInfo.hpp"
+#include "SystemLog.hpp"
+#include "SystemGraphics.hpp"
+#include "MessageUse.hpp"
+#include "ComponentCamera.hpp"
+#include "Object.hpp"
+
+#include <OGRE/OgreSceneManager.h>
+#include <OGRE/OgreRay.h>
+
+using namespace ENGINE;
+
+TypeInfo *ComponentUse::mType = new TypeInfo("ComponentUse", &ComponentUse::createInstance);
+
+ComponentUse::ComponentUse(Object *object, ParamMap &params) :
+    Component(object, params, mType)
+{
+    LOG_IN("component");
+    mParent = nullptr;
+    LOG_OUT("component");
+}
+
+ComponentUse::~ComponentUse()
+{
+    LOG_IN("component");
+    LOG_OUT("component");
+}
+
+void *ComponentUse::createInstance(Object *object, ParamMap &params)
+{
+    LOG_IN("component");
+    LOG_OUT("component");
+    return new ComponentUse(object, params);
+}
+
+void ComponentUse::init()
+{
+    LOG_IN("component");
+    for(int i=0; i<mObject->getNumberComponents(); i++)
+    {
+        Component *c = mObject->getComponent(i);
+        if(c->getType() == ComponentCamera::getType())
+        {
+            mParent = (ComponentCamera *)c;
+        }
+    }
+    if(!mParent)
+        LOG("No ComponentCamera found!", "error");
+    LOG_OUT("component");
+}
+    
+void ComponentUse::update(float elapsedTime)
+{
+    LOG_IN_FRAME;
+    LOG_OUT_FRAME;
+}
+
+void ComponentUse::receiveMessage(Message *message)
+{
+    LOG_IN_FRAME;
+    if(message->getID() == MessageUse::getID())
+    {
+        Ogre::Ray ray(mParent->getCamera()->getDerivedPosition(), mParent->getCamera()->getDerivedDirection());
+        Ogre::RaySceneQuery *raySceneQuery = SystemGraphics::getSingleton()->getSceneMgr()->createRayQuery(ray);
+        raySceneQuery->setRay(ray);
+        raySceneQuery->setSortByDistance(true);
+
+        Ogre::RaySceneQueryResult &result = raySceneQuery->execute();
+        Ogre::RaySceneQueryResult::iterator i = result.begin();
+
+        while(i!=result.end())
+        {
+            if(i->distance > 10)
+                break;
+
+            if(i->movable && i->movable->getMovableType() == "Entity")
+            {
+                Object *obj = Ogre::any_cast<Object *>(i->movable->getUserObjectBindings().getUserAny("Object"));
+                if(obj && obj != mObject)
+                {
+                    LOG("use " + obj->getName(), "component");
+                    MessageUse msg;
+                    msg.sendTo(obj);
+                    break;
+                }
+            }
+            ++i;
+        }
+        SystemGraphics::getSingleton()->getSceneMgr()->destroyQuery(raySceneQuery);
+    }
+    LOG_OUT_FRAME;
+}
