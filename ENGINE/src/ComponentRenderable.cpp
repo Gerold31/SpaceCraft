130e35bd91af22db7d1121d18859d430238f5f5f
Gerold31 <kramer.leroy@gmail.com>
2014-05-24 11:02:13 +0200

    add Param MaterialName to ComponentRenderable

--- ENGINE/src/ComponentRenderable.cpp
+++ ENGINE/src/ComponentRenderable.cpp
@@ -32,13 +32,21 @@ void *ComponentRenderable::createInstance(Object *object, ParamMap &params)
     return new ComponentRenderable(object, params);
 }
 
-void ComponentRenderable::init()
+bool ComponentRenderable::init()
 {
     LOG_IN("component");
     mEntity = ((SystemGraphics *)SystemGraphics::getSingleton())->getSceneMgr()->createEntity(mObject->getName() + "RenderableEnt", boost::any_cast<std::string>(mParams["MeshName"]));
     mEntity->getUserObjectBindings().setUserAny("Object", Ogre::Any(mObject));
     SystemGraphics::getSingleton()->getSceneMgr()->getSceneNode(mObject->getName() + boost::any_cast<std::string>(mParams["NodeName"]))->attachObject(mEntity);
+
+    if(mParams.count("MaterialName"))
+    {
+        mEntity->setMaterialName(boost::any_cast<std::string>(mParams["MaterialName"]));
+    }
+
+    mReady = true;
     LOG_OUT("component");
+    return true;
 }
     
 void ComponentRenderable::update(float elapsedTime)
